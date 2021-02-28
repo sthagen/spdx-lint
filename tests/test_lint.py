f080f0df9fed54f081bfe5c78f2ad5a1abaaf901
Stefan Hagen <stefan@hagen.link>
2021-02-28 17:56:40 +0100

    Changed name of negative test case

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -12,7 +12,7 @@ EMPTY_JSON_OBJECT_PATH = pathlib.Path(EXAMPLES_PATH, "empty_object.json")
 SPDX_TYPICAL_JSON_2_2_PATH = pathlib.Path(EXAMPLES_PATH, "spdx-v2.2_sbom.json")
 
 
-def test_spdx_validation_of_json_empty_object():
+def test_spdx_validation_nok_of_json_empty_object():
     with open(EMPTY_JSON_OBJECT_PATH, "rt") as handle:
         sbom_doc = json.load(handle)
     assert spl.spdx_dci_is_valid(sbom_doc) is False
