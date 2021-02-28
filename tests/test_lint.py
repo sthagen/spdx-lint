18b848975dfcf021a8cf505f4ddb8f47be50b6b6
Stefan Hagen <stefan@hagen.link>
2021-02-28 16:26:58 +0100

    Added first negative test on empty object

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring,unused-import,reimported
+import json
 import pathlib
 import pytest  # type: ignore
+
+EXAMPLES_PATH = pathlib.Path("examples")
+EMPTY_JSON_OBJECT_PATH = pathlib.Path(EXAMPLES_PATH, "empty_object.json")
+
+SPDX_TYPICAL_JSON_2_2_PATH = pathlib.Path(EXAMPLES_PATH, "spdx-v2.2_sbom.json")
+
+
+def test_spdx_validation_of_json_empty_object():
+    with open(EMPTY_JSON_OBJECT_PATH, "rt") as handle:
+        sbom_doc = json.load(handle) 
+    assert sbom_doc.get("SPDXID", None) is None
