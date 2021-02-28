ab8ddd8e67b66c55869dd2bf5d30ac39568afeb5
Stefan Hagen <stefan@hagen.link>
2021-02-28 17:56:05 +0100

    Changed test to call new shallow DCI validator

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -4,6 +4,8 @@ import json
 import pathlib
 import pytest  # type: ignore
 
+import spdx_lint.lint as spl
+
 EXAMPLES_PATH = pathlib.Path("examples")
 EMPTY_JSON_OBJECT_PATH = pathlib.Path(EXAMPLES_PATH, "empty_object.json")
 
@@ -12,5 +14,5 @@ SPDX_TYPICAL_JSON_2_2_PATH = pathlib.Path(EXAMPLES_PATH, "spdx-v2.2_sbom.json")
 
 def test_spdx_validation_of_json_empty_object():
     with open(EMPTY_JSON_OBJECT_PATH, "rt") as handle:
-        sbom_doc = json.load(handle) 
-    assert sbom_doc.get("SPDXID", None) is None
+        sbom_doc = json.load(handle)
+    assert spl.spdx_dci_is_valid(sbom_doc) is False
