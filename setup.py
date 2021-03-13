09fb4b0cf532d7ef16429c4d5e7f575cdab92faa
Stefan Hagen <stefan@hagen.link>
2021-03-13 08:01:11 +0100

    Added future requirements to setup tools interface

--- setup.py
+++ setup.py
@@ -31,6 +31,9 @@ setup(
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     install_requires=[
+        "attrs",
+        "click",
+        "jsonschema",
         "xmlschema",
     ],
     entry_points={
