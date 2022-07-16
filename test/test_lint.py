1794c297d9d8de5b254b2d2fa153ed8d077d01fc
Stefan Hagen <stefan@hagen.link>
2022-07-16 15:12:07 +0200

    Presumed linter happiness, again

--- test/test_lint.py
+++ test/test_lint.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=missing-docstring,unused-import,reimported
 import copy
 import json
 import pathlib
@@ -7,20 +6,20 @@ import pytest  # type: ignore
 
 import spdx_lint.lint as spl
 
-EXAMPLES_PATH = pathlib.Path("examples")
-EMPTY_JSON_OBJECT_PATH = pathlib.Path(EXAMPLES_PATH, "empty_object.json")
+EXAMPLES_PATH = pathlib.Path('examples')
+EMPTY_JSON_OBJECT_PATH = pathlib.Path(EXAMPLES_PATH, 'empty_object.json')
 
-SPDX_TYPICAL_JSON_2_2_PATH = pathlib.Path(EXAMPLES_PATH, "spdx-v2.2_sbom.json")
+SPDX_TYPICAL_JSON_2_2_PATH = pathlib.Path(EXAMPLES_PATH, 'spdx-v2.2_sbom.json')
 
 
 def test_spdx_validation_nok_of_json_empty_object():
-    with open(EMPTY_JSON_OBJECT_PATH, "rt") as handle:
+    with open(EMPTY_JSON_OBJECT_PATH, 'rt') as handle:
         sbom_doc = json.load(handle)
     assert spl.spdx_dci_is_valid(sbom_doc) is False
 
 
 def test_spdx_validation_ok_of_typical_json_documentt(capsys):
-    with open(SPDX_TYPICAL_JSON_2_2_PATH, "rt") as handle:
+    with open(SPDX_TYPICAL_JSON_2_2_PATH, 'rt') as handle:
         sbom_doc = json.load(handle)
     assert spl.spdx_dci_is_valid(sbom_doc) is True
     out, _ = capsys.readouterr()
