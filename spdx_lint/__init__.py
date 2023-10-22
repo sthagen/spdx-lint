c20bd1770a90133f2b1f1fcda73460c18715c67a
Stefan Hagen <stefan@hagen.link>
2023-10-22 15:41:17 +0200

    Bumped impl version for release 2023.10.22

--- spdx_lint/__init__.py
+++ spdx_lint/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2023.10.22'
-# [[[end]]]
+__version__ = '2023.10.22+parent.e8a57708'
+# [[[end]]] (checksum: c71a81085241078e6697f1bfe3d3eb9c)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
